d2c7d7d48f9868ed35341ee3c296290e0dbb8537
CharlesYWL <xlyin@ucdavis.edu>
2019-03-12 13:47:41 -0700

    now can detact change

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp()
 
  // start writing firebase functions
  // https://firebase.google.com/docs/functions/typescript
 
-export const helloworld = functions.https.onRequest((request, response) => {
-    console.log('Hello');
-    response.send("hello firebase!");
- });
+export const NewLikeNotification = functions.database.ref('/posts/{owner_id}/{post_it}/likes/{user_id}')
+    .onCreate((snapshot, context) => {
+        const ownerId = context.params.owner_id
+        const userId = context.params.user_id
+        console.log(`New like ${ownerId} by ${userId}`)
+
+        return null
+        })
