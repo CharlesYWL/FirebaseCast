2f9535f35a8f63ea0f528d3039eed3446b9f2b3a
CharlesYWL <xlyin@ucdavis.edu>
2019-03-13 09:07:59 -0700

    can get name, but only app is frontend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,29 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
-
+var name:string
  // start writing firebase functions
  // https://firebase.google.com/docs/functions/typescript
 
-export const NewLikeNotification =  functions.database.ref('/posts/{owner_id}/{post_it}/likes/{user_id}')
+export const NewLikeNotification = functions.database.ref('/posts/{owner_id}/{post_it}/likes/{user_id}')
     .onCreate((snapshot, context) => {
         const ownerId = context.params.owner_id
         const userId = context.params.user_id
+
+        getUsername(userId).then(function (val) {
+            console.log(val+' should be the liker')
+            name = val
+            console.log('after name = val, name is ' + name)
+            return null
+        }).catch (function (err) {
+            console.error(err)
+        })
+
+
         const payload = {
             notification: {
                 title: 'Some one likes your posts',
-                body: userId + ' dianzan'
+                body: name + ' dianzan'
             }
         }
 
@@ -27,9 +38,19 @@ export const NewLikeNotification =  functions.database.ref('/posts/{owner_id}/{p
     });
 
 //return temp promise and should work in .then(function(val) {...})
- function getFcmtoken(uid:string) {
+ async function getFcmtoken(uid:string) {
     return admin.database()
         .ref(`/users/${uid}/fcmtoken`)
+        .once('value')
+        .then(snapshot => {
+            const temp = snapshot.val()
+            return temp
+        })
+}
+
+async function getUsername(uid: string) {
+    return admin.database()
+        .ref(`/users/${uid}/username`)
         .once('value')
         .then(snapshot => {
             const temp = snapshot.val()
