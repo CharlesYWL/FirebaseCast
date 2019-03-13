a6151d84d39ad0144f1e8b3be490dabe310b3c8b
CharlesYWL <xlyin@ucdavis.edu>
2019-03-12 22:36:00 -0700

    can sent FCM, but 1min delay and cannot show uername

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,23 +5,34 @@ admin.initializeApp()
  // start writing firebase functions
  // https://firebase.google.com/docs/functions/typescript
 
-export const NewLikeNotification = functions.database.ref('/posts/{owner_id}/{post_it}/likes/{user_id}')
+export const NewLikeNotification =  functions.database.ref('/posts/{owner_id}/{post_it}/likes/{user_id}')
     .onCreate((snapshot, context) => {
         const ownerId = context.params.owner_id
         const userId = context.params.user_id
+        const payload = {
+            notification: {
+                title: 'Some one likes your posts',
+                body: userId + ' dianzan'
+            }
+        }
 
-        const fcmtoken = getFcmtoken(ownerId)
-        console.log(`${fcmtoken} likes your posts`)
-        console.log(`${userId} likes your posts`)
-
+        getFcmtoken(ownerId).then(function (val) {
+            console.log(val + ' should recive message')
+            return admin.messaging().sendToDevice(val,payload)
+        }).catch(function (err) {
+            console.error(err)
+            })
+    
         return null
     });
 
-function getFcmtoken(uid:string) {
+//return temp promise and should work in .then(function(val) {...})
+ function getFcmtoken(uid:string) {
     return admin.database()
-        .ref(`/users/${uid}/username`)
+        .ref(`/users/${uid}/fcmtoken`)
         .once('value')
         .then(snapshot => {
-            return snapshot.val()
+            const temp = snapshot.val()
+            return temp
         })
 }
